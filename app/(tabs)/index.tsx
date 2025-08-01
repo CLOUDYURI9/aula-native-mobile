6e819cc5efd4ee8723cc3ceaaef687e376cae743
CLOUDYURI9 <yuridinato@gmail.com>
2025-08-01 13:42:35 -0300

    Aplicação básica de conceitos do native react

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, TextInput, Button } from 'react-native';
+import { use, useState } from 'react';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { blue } from 'react-native-reanimated/lib/typescript/Colors';
 
 export default function TabOneScreen() {
+  const [nome, atualizaNome] = useState('Nome');
+  const [sobrenome, atualizaSobrenome] = useState('Sobrenome');
+  const [cpf, atualizaCpf] = useState('CPF');
+  const [email, atualizaEmail] = useState('exemplo@mail.com');
+  const [telefone, atualizaTelefone] = useState('(11) 3325-5577');
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Aba um</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+            <TextInput
+              value={nome}
+              onChangeText={atualizaNome}
+              inputMode='text'
+            />
+            <TextInput
+              value={sobrenome}
+              onChangeText={atualizaSobrenome}
+              inputMode='text'
+            />
+            <TextInput
+              value={cpf}
+              onChangeText={atualizaCpf}
+              inputMode='numeric'
+            />
+            <TextInput
+      
+              value={telefone}
+              onChangeText={atualizaTelefone}
+              inputMode='numeric'
+            />
+            <TextInput
+        
+              value={email}
+              onChangeText={atualizaEmail}
+              inputMode='email'
+            />
+            <Button
+            title='ENVIAR'
+            onPress={() => { alert(nome + ' ' + sobrenome + ' ' + cpf + ' ' + telefone + ' ' + email ) }}
+            />
     </View>
   );
 }
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,45 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Image, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  const [texto, atualizaTexto] = useState('Olá mundo!');
+  const [email, atualizaEmail] = useState('exemplo@mail.com');
+  const [telefone, atualizaTelefone] = useState('(11) 3325-5577');
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Aba Dois</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Image
+        style={{
+          width: 50,
+          height: 50
+        }}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png'
+        }}
+      />
+      <TextInput
+        value={texto}
+        onChangeText={atualizaTexto}
+      />
+      <TextInput
+
+        value={telefone}
+        onChangeText={atualizaTelefone}
+        inputMode='numeric'
+      />
+      <TextInput
+  
+        value={email}
+        onChangeText={atualizaEmail}
+        inputMode='email'
+      />
+      <Button
+      title='ENVIAR'
+      onPress={() => { alert(texto + ' ' + telefone ) }}
+      />
     </View>
   );
 }
@@ -22,6 +53,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
   separator: {
     marginVertical: 30,
